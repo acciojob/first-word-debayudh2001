837ce618d9f5f4d050d8e72545d4b019842a5dc1
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2024-12-27 15:42:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+	let new_str = ""
+	for(let v of str){
+		if(v == " "){
+			break
+		}else{
+			new_str += v
+		}
+	}
+	return new_str
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
+
 alert(firstWord(s));
